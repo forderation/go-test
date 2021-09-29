7e6ffd3f83f2c5a124d538edcec6ff6730800a94
forderation <kharisma.muzaki@gmail.com>
2021-09-29 18:40:43 +0700

    reflection test

--- reflection_test.go
+++ reflection_test.go
@@ -1,5 +1,193 @@
 package main
 
-func Walk(x interface{}, fn func(string)) {
+import (
+	"reflect"
+	"testing"
+)
 
+func Walk(x interface{}, fn func(string)) {
+	val := GetValue(x)
+	valuesNum := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.Struct:
+		valuesNum = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		valuesNum = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			Walk(val.MapIndex(key).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			Walk(v.Interface(), fn)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			Walk(res.Interface(), fn)
+		}
+	}
+
+	for i := 0; i < valuesNum; i++ {
+		Walk(getField(i).Interface(), fn)
+	}
+}
+
+/**
+Get the value reflect
+if kind is pointer x will be convert to element
+**/
+func GetValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
+}
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+
+	var got []string
+	x := struct {
+		Name string
+	}{expected}
+
+	Walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
+}
+
+func TestCaseWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name:          "Struct with one string field",
+			Input:         Person{"Chris", Profile{33, "London"}},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Manchester"},
+			},
+			[]string{"London", "Manchester"},
+		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		Walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channel", func(t *testing.T) {
+		aChannel := make(chan Profile)
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		Walk(aChannel, func(s string) {
+			got = append(got, s)
+		})
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+		Walk(aFunction, func(s string) {
+			got = append(got, s)
+		})
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t testing.TB, hayStack []string, needle string) {
+	t.Helper()
+	constains := false
+	for _, x := range hayStack {
+		if x == needle {
+			constains = true
+		}
+	}
+	if !constains {
+		t.Errorf("expected %+v to contain %q but it didn't", hayStack, needle)
+	}
 }
